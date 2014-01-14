7e3fc9aa960bb9d94c88ca9b078d0b8bce6c4edb
PeteGoo <pete@petegoo.com>
2014-01-14 23:32:58 +1300

    Fix start options

--- MMBot.Router.Relay/RelayRouter.cs
+++ MMBot.Router.Relay/RelayRouter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using MMBot.Router.Nancy;
+using Owin;
 using SignalR.Owin.Relay.Client;
 
 namespace MMBot.Router.Relay
@@ -31,9 +32,19 @@ namespace MMBot.Router.Relay
             {
                 throw new InvalidOperationException("The router has not yet been configured. You must call Configure before calling start");
             }
-            _client = RelayClient.Create<Startup>(string.Format("http://{0}:{1}/relay", _hostname, _port));
+            var relayUri = string.Format("http://{0}:{1}/relay", _hostname, _port);
+            try
+            {
+                _client = RelayClient.Create(app => app.UseNancy(options => options.Bootstrapper = new Bootstrapper(this)), relayUri);
 
-            _client.Start();
+                _client.Start();
+
+                _robot.Logger.Info("The relay router is connected to " + relayUri);
+            }
+            catch (Exception e)
+            {
+                _robot.Logger.Error("Could not connect to relay router " + relayUri, e);
+            }
         }
 
         public override void Stop()
